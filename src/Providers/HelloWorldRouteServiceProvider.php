7e49e4064ecb83c6a129e04a4dd215c3f0bff16e
schwertbanner <info@schwertbanner.de>
2017-07-18 15:33:14 +0200

    hmm
    Hello World Ding

---
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace HelloWorld\Providers;
+
+
+use Plenty\Plugin\RouteServiceProvider;
+use Plenty\Plugin\Routing\Router;
+
+class HelloWorldRouteServiceProvider extends RouteServiceProvider
+{
+    public function map(Router $router)
+    {
+
+    }
+}
+public function register()
+{
+    $this->getApplication()->register(HelloWorldRouteServiceProvider::class);
+}
+public function map(Router $router)
+{
+    $router->get('hello','HelloWorld\Controllers\ContentController@sayHello');
+}
